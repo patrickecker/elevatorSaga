c3197f9339583e81b7e6ef697421eb8704ad19f1
Patrick Ecker <patrick.s.ecker@gmail.com>
2020-08-18 12:52:51 -0500

    Queue processing and Indicators
    Added indicators to assist passengers to properly enter elevator in 
    direction of travel.
    Added queue processing to avoid duplicate stops in current direction.

--- solution.js
+++ solution.js
@@ -10,6 +10,31 @@
               // Maybe tell the elevator to go to that floor?
               elevator.goToFloor(floorNum);
           });
+
+          elevator.on("passing_floor", function(floorNum, direction) {
+              // check to see if floorNum is in the destinationQueue
+              if (elevator.destinationQueue.indexOf(floorNum) != -1) {
+                  var index = elevator.destinationQueue.indexOf(floorNum);
+                  elevator.destinationQueue.splice(index, 1);
+                  elevator.goToFloor(floorNum, true);
+              }
+          });
+
+          elevator.on("stopped_at_floor", function(floorNum) {
+              // set the car's direction indicators for passengers
+              if (floorNum == 0) {
+                  elevator.goingUpIndicator(true);
+                  elevator.goingDownIndicator(false);
+                  currentDirection = "up";
+              } else if (floorNum == (floors.length - 1)) {
+                  elevator.goingUpIndicator(false);
+                  elevator.goingDownIndicator(true);
+                  currentDirection = "down";
+              } else {
+                  elevator.goingUpIndicator(true);
+                  elevator.goingDownIndicator(true);
+              };
+          });
         });
     },
     update: function(dt, elevators, floors) {
